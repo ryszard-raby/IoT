de9e7bbcd0d26b5f7548c531668e4731071f72a1
NISZCZYCIEL\Richart <ryszard.raby>
2018-12-27 22:02:19 +0100

    update

--- Arduino/Blynk [OTA WiFiManager]/src/main.cpp
+++ Arduino/Blynk [OTA WiFiManager]/src/main.cpp
@@ -1,42 +1,26 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-#define BLYNK_PRINT Serial
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+//needed for library
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-
-#include <C:/auth/auth.h>
 #include <C:/auth/blynkToken.h>
 
-BlynkTimer timer;
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = Token_BlynkOTA;
+char blynk_token[33] = Token_BlynkWiFiOTA;
+//#include <BlynkSimpleEsp8266.h>
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = AuthSsid;
-char pass[] = AuthPass;
+// select which pin will trigger the configuration portal when set to LOW
+// ESP-01 users please note: the only pins available (0 and 2), are shared 
+// with the bootloader, so always set them HIGH at power-up
+#define TRIGGER_PIN 4
 
-void OTAsetup(){
-// Debug console
-
-  ArduinoOTA.setHostname("ArduinoOta");
-
-  // No authentication by default
-  ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.onStart([]() {
+void otaSetup(){
+    ArduinoOTA.setHostname("myesp8266");
+    ArduinoOTA.setPassword("admin");
+    ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -73,40 +57,51 @@ void OTAsetup(){
   Serial.println(WiFi.localIP());
 }
 
-void programSetup(){
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFiManager wifiManager;
-  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", auth, 32);
-  wifiManager.autoConnect("AutoConnectAP");
-  
-  // pinMode(2, OUTPUT);
+  Serial.println("\n Starting");
+  otaSetup();
+  pinMode(TRIGGER_PIN, INPUT);
 }
 
-void program(){
-  Blynk.virtualWrite(V0, AuthSsid);
-  Blynk.virtualWrite(V1, AuthPass);
-  Blynk.virtualWrite(V2, Token_BlynkOTA);
-}
+void loop() {
+  // is configuration portal requested?
+  if ( digitalRead(TRIGGER_PIN) == LOW ) {
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 34);
+    WiFiManager wifiManager;
+    wifiManager.addParameter(&custom_blynk_token);
+    //reset settings - for testing
+    //wifiManager.resetSettings();
 
-void setup()
-{
-  OTAsetup();
-  programSetup();
-  timer.setInterval(1000L, program);
-}
+    //sets timeout until configuration portal gets turned off
+    //useful to make it all retry or go to sleep
+    //in seconds
+    //wifiManager.setTimeout(120);
 
-void loop()
-{
-  ArduinoOTA.handle();
-  Blynk.run();
-  timer.run();
-  digitalWrite(12, LOW);
+    //it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
 
-  delay(5000);
-  digitalWrite(12, HIGH);
+    //WITHOUT THIS THE AP DOES NOT SEEM TO WORK PROPERLY WITH SDK 1.5 , update to at least 1.5.1
+    //WiFi.mode(WIFI_STA);
+    
+    if (!wifiManager.startConfigPortal("OnDemandAP")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.reset();
+      delay(5000);
+    }
 
-  delay(100);
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    //strcpy(blynk_token, custom_blynk_token.getValue());
+  }
+
+    ArduinoOTA.handle();
+    digitalWrite(12, HIGH);
+  // put your main code here, to run repeatedly:
 }
