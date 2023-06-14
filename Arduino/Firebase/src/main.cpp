0e43ba06bc13dccef0c7ad9eee2395164b5c1767
Ryszard Raby <ryszard.raby@ceneo.pl>
2023-06-14 09:58:35 +0200

    #master

--- Arduino/Firebase/src/main.cpp
+++ Arduino/Firebase/src/main.cpp
@@ -5,7 +5,8 @@
 #endif
 
 #include <Arduino.h>
-#include <firebaseService.h>
+#include <FirebaseService.h>
+#include <DateTime.h>
 #include <time.h>
 #include <cstdlib>
 
@@ -17,10 +18,8 @@ int cout = 0;
 uint8_t builtInLed = 2;
 
 int intData;
-int delayTime = 0;
 long long int currentTime;
 long long int serverTime;
-bool synchronized = false;
 
 FirebaseService firebaseService;
 
@@ -38,33 +37,39 @@ void connectWiFi() {
   Serial.println();
 }
 
-String fullTime(int millis) {
+String fullTime(long long int millis) {
   int seconds = (millis / 1000) % 60;
   int minutes = (millis / (1000 * 60)) % 60;
   int hours = (millis / (1000 * 60 * 60)) % 24;
   return String(hours) + ":" + String(minutes) + ":" + String(seconds) + ":" + String(millis);
 }
 
-String fullDate(long long int millis) {
+DateTime fullDate(long long int millis) {
   time_t t = millis / 1000;
   struct tm *tm = localtime(&t);
   tm->tm_hour += 2;
   time_t t2 = mktime(tm);
   tm = localtime(&t2);
 
-  return String(tm->tm_year + 1900) + "-" + 
-         String(tm->tm_mon + 1) + "-" + 
-         String(tm->tm_mday) + " " + 
-         String(tm->tm_hour) + ":" + 
-         String(tm->tm_min) + ":" + 
-         String(tm->tm_sec);
+  return DateTime(tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 }
 
-long long int delay(long long int millis) {
-  time_t t = millis / 1000;
-  struct tm *tm = localtime(&t);
-  tm->tm_hour += 2;
-  return ((60 - tm->tm_min % 60 - 1) * 60000) + ((60 - tm->tm_sec % 60 - 1 - 5) * 1000); // 5 seconds before full hour
+void callback(String key, String value) {
+  Serial.println("\nCallbackFunction \n Key: " + key + " Value: " + value + "\n");
+
+  if (key == "time") {
+    serverTime = std::stoll(value.c_str());
+    currentTime = serverTime - millis();
+    DateTime dateTime = fullDate(currentTime + millis());
+
+    Serial.println("Current time: " + String(dateTime.year) + "-" + String(dateTime.month) + "-" + String(dateTime.day) + " " + String(dateTime.hour) + ":" + String(dateTime.minute) + ":" + String(dateTime.second));
+  }
+
+  if (key == "gpio2") {
+    intData = std::stoi(value.c_str());
+    digitalWrite(builtInLed, intData);
+    Serial.println("Pin2: " + String(value));
+  }
 }
 
 void setup() {
@@ -75,21 +80,7 @@ void setup() {
   connectWiFi();
   firebaseService.connectFirebase();
   firebaseService.setCallback([](String key, String value) {
-    // Serial.println("\nCallbackFunction \n Key: " + key + " Value: " + value + "\n");
-
-    if (key == "time") {
-      serverTime = std::stoll(value.c_str());
-      currentTime = serverTime - millis();
-      
-      Serial.println("Server time: " + fullDate(serverTime));
-      Serial.println("Delay: " + fullTime(delay(serverTime)));
-    }
-
-    if (key == "gpio2") {
-      intData = std::stoi(value.c_str());
-      digitalWrite(builtInLed, intData);
-      Serial.println("Pin2: " + String(value));
-    }
+    callback(key, value);
   });
   firebaseService.setTimestamp();
 }
@@ -97,14 +88,9 @@ void setup() {
 void loop() {
   firebaseService.firebaseStream();
   
-  if (millis() - secTemp > 1000) {
-    Serial.println(" Current time: " + fullDate(currentTime + millis()));
-    secTemp = millis();
-  }
-
-  if (millis() - minTemp >= delay(serverTime)) {
+  if (millis() - secTemp > 60000) {
     firebaseService.setTimestamp();
-    minTemp = millis();
-    Serial.println("Synchronized");
+    secTemp = millis();
+    // Serial.println("Live time: " + fullDate(millis()));
   }
 }
