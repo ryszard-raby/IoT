9337ef92830c10be95050e38025acfa8e2afc436
Ryszard Raby <ryszard.raby@gmail.com>
2025-06-29 08:16:45 +0200

    Pasillo

---
+++ Arduino/Pasillo/src/TimeService.h
@@ -0,0 +1,59 @@
+#include <chrono>
+#include <Timer.h>
+
+using namespace std::chrono;
+
+class TimeService {
+    public:
+
+    system_clock::time_point timeSnapPoint;
+
+    system_clock::time_point snapTimePoint(long long timeStamp) {
+        system_clock::time_point timePoint = system_clock::from_time_t(timeStamp/1000);
+        
+        // UTC+1
+        timePoint += hours(1);
+
+        //get month from timePoint
+        time_t timePoint_t = system_clock::to_time_t(timePoint);
+        auto lt = localtime(&timePoint_t);
+        int month = lt->tm_mon;
+
+        //check if summer time
+        if (month > 3 && month < 10) {
+            timePoint += hours(1);
+        }
+
+        timeSnapPoint = timePoint;
+
+        return timePoint;
+    }
+
+    system_clock::time_point timePointNow(int liveTime) {
+        return timeSnapPoint + seconds(liveTime/1000);
+    }
+
+    Timer timerFromTimePoint(system_clock::time_point timePoint) {
+        Timer Timer;
+        time_t currentTime = system_clock::to_time_t(timePoint);
+        auto lt = localtime(&currentTime);
+        Timer.day = lt->tm_mday;
+        Timer.month = lt->tm_mon;
+        Timer.year = lt->tm_year;
+        Timer.hour = lt->tm_hour;
+        Timer.minute = lt->tm_min;
+        Timer.second = lt->tm_sec;
+        Timer.millisecond = 0;
+        Timer.timePoint = timePoint;
+        return Timer;
+    }
+
+    system_clock::time_point timePointFromTimer(Timer Timer) {
+        time_t currentTime = time(0);
+        auto lt = localtime(&currentTime);
+        lt->tm_hour = Timer.hour;
+        lt->tm_min = Timer.minute;
+        lt->tm_sec = Timer.second;
+        return system_clock::from_time_t(mktime(lt));
+    }
+};
---
+++ Arduino/Pasillo/src/main.cpp
@@ -0,0 +1,145 @@
+#include <Arduino.h>
+#include <WiFiService.h>
+#include <FirebaseService.h>
+#include <TimeService.h>
+#include <chrono>
+#include <time.h>
+#include <Oled.h>
+
+int startTime = 0;
+
+bool updateLog = false;
+bool updateTime = false;
+
+Timer timeNow;
+Timer t_timeNow;
+Timer timer1;
+Timer timer2;
+
+int manual = 0;
+int brightness = 255;
+
+auto timer1TimePoint = system_clock::now();
+auto timer2TimePoint = system_clock::now();
+auto intervalTimePoint = system_clock::now();
+
+Oled oled;
+
+int powerPin = 4;
+
+
+WiFiService wifiService;
+FirebaseService firebaseService;
+TimeService timeService;
+
+system_clock::time_point timeSnapPoint;
+
+void setTimePoint(Timer &timer, Timer &timeNow) {
+  timer.timePoint = timeNow.timePoint;
+
+  time_t currentTime = system_clock::to_time_t(timer.timePoint);
+  struct tm *timeInfo = localtime(&currentTime);
+  timeInfo->tm_hour = timer.hour;
+  timeInfo->tm_min = timer.minute;
+  timeInfo->tm_sec = timer.second;
+  timer.timePoint = system_clock::from_time_t(mktime(timeInfo));
+}
+
+void callback(String key, String value) {
+  if (key == "time") {
+    Serial.println(std::stoll(value.c_str()));
+    timeSnapPoint = timeService.snapTimePoint(std::stoll(value.c_str()));
+  }
+
+  if (key == "timer1") {
+    timer1.hour = std::stoi(value.c_str());
+    timer1.minute = 0;
+    timer1.second = 0;
+    setTimePoint(timer1, timeNow);
+    Serial.println("timer1");
+  }
+
+  if (key == "timer2") {
+    timer2.hour = std::stoi(value.c_str());
+    timer2.minute = 0;
+    timer2.second = 0;
+    setTimePoint(timer2, timeNow);
+    Serial.println("timer2");
+  }
+
+  if (key == "manual") {
+    manual = std::stoi(value.c_str());
+  }
+
+  if (key == "brightness") {
+    brightness = std::stoi(value.c_str());
+    if (brightness < 0) brightness = 0;
+    if (brightness > 255) brightness = 255;
+    analogWrite(LED_BUILTIN, brightness);
+  }
+}
+
+bool trigger(Timer timeNow, Timer &timer1, Timer &timer2) {
+  if (timer1.hour <= timer2.hour) {
+    // Przedział nie przechodzi przez północ
+    return (timeNow.hour >= timer1.hour && timeNow.hour < timer2.hour);
+  } else {
+    // Przedział przechodzi przez północ
+    return (timeNow.hour >= timer1.hour || timeNow.hour < timer2.hour);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  oled.init();
+  oled.add(0, 0, "time", 2);
+  oled.add(0, 25, "timer1", 1);
+  oled.add(60, 25, "timer2", 1);
+  oled.add(0, 40, "0", 2);
+  oled.add(60, 40, "0", 2);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+
+  wifiService.connect();
+  
+  firebaseService.connectFirebase();
+  firebaseService.setCallback([](String key, String value) {
+    callback(key, value);
+  });
+  firebaseService.setTimestamp();
+
+  startTime = millis();
+}
+
+void loop() {
+  firebaseService.firebaseStream();
+  timeNow.timePoint = timeSnapPoint + seconds(millis() / 1000 - startTime / 1000);
+  timeNow = timeService.timerFromTimePoint(timeNow.timePoint);
+
+  String formattedTime = String(timeNow.hour) + ":" + String(timeNow.minute) + ":" + String(timeNow.second);
+  String formattedTimeAndDate = String(timeNow.day) + "/" + String(timeNow.month) + "/" + String(timeNow.year) + " " + formattedTime;
+
+  if (timeNow.second != t_timeNow.second) {
+    t_timeNow.second = timeNow.second;
+
+    setTimePoint(timer1, timeNow);
+    setTimePoint(timer2, timeNow);
+
+    oled.stack[0].text = formattedTime;
+    oled.stack[3].text = String(timer1.hour);
+    oled.stack[4].text = String(timer2.hour);
+    oled.print();
+
+    if (trigger(timeNow, timer1, timer2) || manual == 1) {
+      analogWrite(powerPin, brightness);
+      analogWrite(LED_BUILTIN, 255 - brightness);
+      updateLog = true;
+    }
+    else {
+      digitalWrite(powerPin, LOW);
+      analogWrite(LED_BUILTIN, 255);
+    }
+  }
+}
